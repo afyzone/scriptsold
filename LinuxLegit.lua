b9002d6b40ec7b2f8b6f70829d6d0901470e2a25
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-01-28 17:38:06 -0800

    Update LinuxLegit.lua

--- LinuxLegit.lua
+++ LinuxLegit.lua
@@ -1,9 +1,10 @@
 -- // Dependencies
 _G.PRED = 0.037
-local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/Aiming/main/GamePatches/Module/2788229376.lua"))()
+local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Universal/Aiming/GamePatches/2788229376.lua"))()
 Aiming.TeamCheck(false)
 Aiming.ShowFOV = false
 Aiming.FOV = 25
+Aiming.ShowCredits = False
 -- // Services
 local Workspace = game:GetService("Workspace")
 local Players = game:GetService("Players")
