1838a7bba5e3ed73ec5a4f788a9bd7cc667ef794
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-02-17 09:01:36 -0800

    Update budokai.lua

--- budokai.lua
+++ budokai.lua
@@ -83,7 +83,7 @@ spawn(function()
     end
 end)
 spawn(function()
-    while wait(500) do
+    while wait(60) do
         if game.PlaceId == 8675599791 then
             getquest()
             wait(5)
