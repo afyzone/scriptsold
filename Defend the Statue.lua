7f565ab9b0b2c7b44a037b3857024656db55805e
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-02-16 17:28:10 -0800

    Update Defend the Statue.lua

--- Defend the Statue.lua
+++ Defend the Statue.lua
@@ -24,7 +24,18 @@ b:Button("Noclip",function()
 end)
 
 b:Button("Click to copy Discord Link!",function()
-  setclipboard("https://discord.gg/BZUGjvYH")
+    if syn.request or http_request or request or http.request then
+        local req = syn.request or http_request or request or http.request or nil
+        if req ~= nil then
+            for port=6463, 6472, 1 do
+                local inv = "http://127.0.0.1:"..tostring(port).."/rpc?v=1"
+                local http = game:GetService("HttpService")
+                local t = {cmd = "INVITE_BROWSER", args = {["code"] = "9YrrVdmfxG"}, nonce = string.lower(http:GenerateGUID(false))}
+                local post = http:JSONEncode(t)
+                req({Url = inv, Method = "POST", Body = post, Headers = {["Content-Type"] = "application/json", ["Origin"] = "https://discord.com"}})
+            end
+        end
+    end
 end)
 
 while wait() do
