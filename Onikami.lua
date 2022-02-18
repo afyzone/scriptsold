4196f05f777c9eae346144b29c8d63a38047d4b0
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-02-17 16:31:06 -0800

    Update Onikami.lua

--- Onikami.lua
+++ Onikami.lua
@@ -11,10 +11,11 @@ b:Toggle("Auto Farm",function(bool)
     Settings.autofarm = bool
 end)
 b:Button("Reset",function()
-    game:GetService("ReplicatedStorage").Remotes.BindReset:FireServer()
+    game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].Humanoid.Health = 0
 end)
 
 b:Button("Click to copy Discord Link!",function()
+    setclipboard("https://discord.gg/9YrrVdmfxG")
     if syn.request or http_request or request or http.request then
         local req = syn.request or http_request or request or http.request or nil
         if req ~= nil then
@@ -28,7 +29,7 @@ b:Button("Click to copy Discord Link!",function()
         end
     end
 end)
-b:Label("leadmarker kinda cute",{
+b:Label("LeadMarker kinda cute",{
     TextSize = 20; -- Self Explaining
     TextColor = Color3.fromRGB(255,255,255); -- Self Explaining
     BgColor = Color3.fromRGB(69,69,69); -- Self Explaining
@@ -38,20 +39,38 @@ while wait() do
     if Settings.autofarm then
         pcall(function()
             for i, v in pairs(game:GetService("Workspace").LivingThings:GetChildren()) do
-                if game:GetService("Players").LocalPlayer.Quest.IsActive.Value == false then
-                    game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = game:GetService("Workspace").IntNpcs.MissionD.HumanoidRootPart.CFrame
-                    wait(.3)
-                    game:GetService("ReplicatedStorage").Remotes.GiverQuest:FireServer("MissionDemon")
-                end
-                if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Fists") then
-                    game.Players.LocalPlayer.Character.Humanoid:EquipTool(game:GetService("Players").LocalPlayer.Backpack.Fists)
-                end
-                if v.Name == "Slayer"  and v.Humanoid.Health > 0 then
-                    repeat wait()
-                    game:GetService("ReplicatedStorage").Remotes.CombatRemotes.Fists:FireServer("Release")
-                    game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.Velocity = Vector3.new(0,0,0)
-                    game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = v:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0,0,8)
-                    until not Settings.autofarm or v.Humanoid.Health <= 0
+                if game:GetService("Players").LocalPlayer.leaderstats.Rank.Value == "Demon" then
+                    if game:GetService("Players").LocalPlayer.Quest.IsActive.Value == false then
+                        game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = game:GetService("Workspace").IntNpcs.MissionD.HumanoidRootPart.CFrame
+                        wait(.3)
+                        game:GetService("ReplicatedStorage").Remotes.GiverQuest:FireServer("MissionDemon")
+                    end
+                    if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Fists") then
+                        game.Players.LocalPlayer.Character.Humanoid:EquipTool(game:GetService("Players").LocalPlayer.Backpack.Fists)
+                    end
+                    if v.Name == "Slayer"  and v.Humanoid.Health > 0 then
+                        repeat wait()
+                        game:GetService("ReplicatedStorage").Remotes.CombatRemotes.Fists:FireServer("Release")
+                        game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.Velocity = Vector3.new(0,0,0)
+                        game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = v:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0,0,8)
+                        until not Settings.autofarm or v.Humanoid.Health <= 0
+                    end
+                elseif game:GetService("Players").LocalPlayer.leaderstats.Rank.Value == "Slayer" then
+                    if game:GetService("Players").LocalPlayer.Quest.IsActive.Value == false then
+                        game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = game:GetService("Workspace").IntNpcs.Mission.HumanoidRootPart.CFrame
+                        wait(.3)
+                        game:GetService("ReplicatedStorage").Remotes.GiverQuest:FireServer("Mission")
+                    end
+                    if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Fists") then
+                        game.Players.LocalPlayer.Character.Humanoid:EquipTool(game:GetService("Players").LocalPlayer.Backpack.Fists)
+                    end
+                    if v.Name == "Demon"  and v.Humanoid.Health > 0 then
+                        repeat wait()
+                        game:GetService("ReplicatedStorage").Remotes.CombatRemotes.Fists:FireServer("Release")
+                        game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.Velocity = Vector3.new(0,0,0)
+                        game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = v:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0,0,8)
+                        until not Settings.autofarm or v.Humanoid.Health <= 0
+                    end
                 end
             end
         end)
