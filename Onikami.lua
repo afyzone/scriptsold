2d29a03858abdcc4db0ca3269eaafdac61881b44
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-02-17 17:27:34 -0800

    Update Onikami.lua

--- Onikami.lua
+++ Onikami.lua
@@ -10,10 +10,41 @@ local Settings = {}
 b:Toggle("Auto Farm",function(bool)
     Settings.autofarm = bool
 end)
+b:Button("Infinite Breathing",function()
+    while wait() do
+        game:GetService("ReplicatedStorage").Remotes.StartBreathing:FireServer()
+    end
+end)
 b:Button("Reset",function()
     game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].Humanoid.Health = 0
 end)
 
+b:Label("Teleports:",{
+    TextSize = 20; -- Self Explaining
+    TextColor = Color3.fromRGB(255,255,255); -- Self Explaining
+    BgColor = Color3.fromRGB(69,69,69); -- Self Explaining
+})
+b:Button("WisteriaEstate",function()
+    game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = game:GetService("Workspace").Regions.WisteriaEstate.CFrame
+end)
+b:Button("SwordSmith",function()
+    game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = game:GetService("Workspace").Regions.SwordSmith.CFrame
+end)
+b:Button("Kunohe",function()
+    game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = game:GetService("Workspace").Regions.Kunohe.CFrame
+end)
+b:Button("SlayersHQ",function()
+    game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = game:GetService("Workspace").Regions.SlayersHQ.CFrame
+end)
+b:Button("District",function()
+    game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = game:GetService("Workspace").Regions.District.CFrame
+end)
+
+b:Label("LeadMarker kinda cute",{
+    TextSize = 20; -- Self Explaining
+    TextColor = Color3.fromRGB(255,255,255); -- Self Explaining
+    BgColor = Color3.fromRGB(69,69,69); -- Self Explaining
+})
 b:Button("Click to copy Discord Link!",function()
     setclipboard("https://discord.gg/9YrrVdmfxG")
     if syn.request or http_request or request or http.request then
@@ -29,11 +60,6 @@ b:Button("Click to copy Discord Link!",function()
         end
     end
 end)
-b:Label("LeadMarker kinda cute",{
-    TextSize = 20; -- Self Explaining
-    TextColor = Color3.fromRGB(255,255,255); -- Self Explaining
-    BgColor = Color3.fromRGB(69,69,69); -- Self Explaining
-})
 
 while wait() do
     if Settings.autofarm then
@@ -48,14 +74,24 @@ while wait() do
                     if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Fists") then
                         game.Players.LocalPlayer.Character.Humanoid:EquipTool(game:GetService("Players").LocalPlayer.Backpack.Fists)
                     end
-                    if v.Name == "Slayer"  and v.Humanoid.Health > 0 then
+                    if v.Name == "Slayer" and v.Humanoid.Health > 0 then
                         repeat wait()
                         game:GetService("ReplicatedStorage").Remotes.CombatRemotes.Fists:FireServer("Release")
                         game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.Velocity = Vector3.new(0,0,0)
-                        game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = v:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0,0,8)
+                        game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = v:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0,0,4)
                         until not Settings.autofarm or v.Humanoid.Health <= 0
                     end
                 elseif game:GetService("Players").LocalPlayer.leaderstats.Rank.Value == "Civilian" then
+                    if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Fists") then
+                        game.Players.LocalPlayer.Character.Humanoid:EquipTool(game:GetService("Players").LocalPlayer.Backpack.Fists)
+                    end
+                    if v.Name == "Demon" and v.Humanoid.Health > 0 then
+                        repeat wait()
+                        game:GetService("ReplicatedStorage").Remotes.CombatRemotes.Fists:FireServer("Release")
+                        game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.Velocity = Vector3.new(0,0,0)
+                        game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = v:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0,0,4)
+                        until not Settings.autofarm or v.Humanoid.Health <= 0
+                elseif game:GetService("Players").LocalPlayer.leaderstats.Rank.Value == "Slayer" then
                     if game:GetService("Players").LocalPlayer.Quest.IsActive.Value == false then
                         game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = game:GetService("Workspace").IntNpcs.Mission.HumanoidRootPart.CFrame
                         wait(.3)
@@ -64,14 +100,15 @@ while wait() do
                     if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Fists") then
                         game.Players.LocalPlayer.Character.Humanoid:EquipTool(game:GetService("Players").LocalPlayer.Backpack.Fists)
                     end
-                    if v.Name == "Demon"  and v.Humanoid.Health > 0 then
+                    if v.Name == "Demon" and v.Humanoid.Health > 0 then
                         repeat wait()
                         game:GetService("ReplicatedStorage").Remotes.CombatRemotes.Fists:FireServer("Release")
                         game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.Velocity = Vector3.new(0,0,0)
-                        game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = v:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0,0,8)
+                        game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = v:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0,0,4)
                         until not Settings.autofarm or v.Humanoid.Health <= 0
                     end
                 end
+            end
             end
         end)
     end
