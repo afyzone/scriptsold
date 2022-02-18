a9b815e3920a2262633ad39bfbf88be8b80dd6da
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-02-17 20:05:13 -0800

    Update Onikami.lua

--- Onikami.lua
+++ Onikami.lua
@@ -1,13 +1,15 @@
 local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/AikaV3rm/UiLib/master/Lib.lua')))()
 local w = library:CreateWindow("afy#0679") -- Creates the window
 local b = w:CreateFolder("Onikami") -- Creates the folder(U will put here your buttons,etc)
-local Rank = game:GetService("Players").LocalPlayer.leaderstats.Rank.Value
+local Rankings = game:GetService("Players").LocalPlayer.leaderstats.Rank.Value
 b:Label("HATE FAGS",{
     TextSize = 25; -- Self Explaining
     TextColor = Color3.fromRGB(255,255,255); -- Self Explaining
     BgColor = Color3.fromRGB(69,69,69); -- Self Explaining
 })
 local Settings = {}
+local weaponlist = {}
+
 b:Toggle("Auto Farm",function(bool)
     Settings.autofarm = bool
 end)
@@ -40,6 +42,9 @@ end)
 b:Button("District",function()
     game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = game:GetService("Workspace").Regions.District.CFrame
 end)
+b:Button("Final Selection Start",function()
+    game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = game:GetService("Workspace").IntNpcs.FSEnterHead.Head.CFrame
+end)
 
 b:Label("LeadMarker kinda cute",{
     TextSize = 20; -- Self Explaining
@@ -66,7 +71,7 @@ while wait() do
     if Settings.autofarm then
         pcall(function()
             for i, v in pairs(game:GetService("Workspace").LivingThings:GetChildren()) do
-                if Rank == "Demon" or Rank == "Weak1" or Rank == "Weak2" or Rank == "WeakMed1" or Rank == "WeakMed2" or Rank == "WeakMed3" or Rank == "WeakMed4" or Rank == "Strong1" or Rank == "Strong2" or Rank == "Strong3" or Rank == "Strong4" or Rank == "Lower Moon" then
+                if Rankings == "Demon" or Rankings == "Weak1" or Rankings == "Weak2" or Rankings == "WeakMed1" or Rankings == "WeakMed2" or Rankings == "WeakMed3" or Rankings == "WeakMed4" or Rankings == "Strong1" or Rankings == "Strong2" or Rankings == "Strong3" or Rankings == "Strong4" or Rankings == "Lower Moon" then
                     if game:GetService("Players").LocalPlayer.Quest.IsActive.Value == false then
                         game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = game:GetService("Workspace").IntNpcs.MissionD.HumanoidRootPart.CFrame
                         wait(.3)
@@ -75,6 +80,12 @@ while wait() do
                     if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Fists") then
                         game.Players.LocalPlayer.Character.Humanoid:EquipTool(game:GetService("Players").LocalPlayer.Backpack.Fists)
                     end
+                    if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Old Nichirin Katana") then
+                        game.Players.LocalPlayer.Character.Humanoid:EquipTool(game:GetService("Players").LocalPlayer.Backpack["Old Nichirin Katana"])
+                    end
+                    if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Nichirin Katana") then
+                        game.Players.LocalPlayer.Character.Humanoid:EquipTool(game:GetService("Players").LocalPlayer.Backpack["Nichirin Katana"])
+                    end
                     if v.Name == "Slayer" and v.Humanoid.Health > 0 then
                         repeat wait()
                         game:GetService("ReplicatedStorage").Remotes.CombatRemotes.Fists:FireServer("Release")
@@ -82,12 +93,14 @@ while wait() do
                         game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = v:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0,0,4)
                         until not Settings.autofarm or v.Humanoid.Health <= 0
                     end
-                elseif Rank == "Civilian" then
+                elseif Rankings == "Civilian" then
                     if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Fists") then
                         game.Players.LocalPlayer.Character.Humanoid:EquipTool(game:GetService("Players").LocalPlayer.Backpack.Fists)
-                    elseif game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Old Nichirin Katana") then
+                    end
+                    if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Old Nichirin Katana") then
                         game.Players.LocalPlayer.Character.Humanoid:EquipTool(game:GetService("Players").LocalPlayer.Backpack["Old Nichirin Katana"])
-                    elseif game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Nichirin Katana") then
+                    end
+                    if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Nichirin Katana") then
                         game.Players.LocalPlayer.Character.Humanoid:EquipTool(game:GetService("Players").LocalPlayer.Backpack["Nichirin Katana"])
                     end
                     if v.Name == "Demon" and v.Humanoid.Health > 0 then
@@ -97,28 +110,31 @@ while wait() do
                         game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.Velocity = Vector3.new(0,0,0)
                         game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = v:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0,0,4)
                         until not Settings.autofarm or v.Humanoid.Health <= 0
-                elseif Rank == "Slayer" or Rank == "Mizunoto" or Rank == "Mizunoe" or Rank == "Kanoto" or Rank == "Kanoe" or Rank == "Tsuchinotos" or Rank == "Tsuchinoe" or Rank == "Hinoto" or Rank == "Hinoe" or Rank == "Kinoto" or Rank == "Kinoe" or Rank == "PseudoHashira" then
+                    end
+                elseif Rankings == "Slayer" or Rankings == "Mizunoto" or Rankings == "Mizunoe" or Rankings == "Kanoto" or Rankings == "Kanoe" or Rankings == "Tsuchinotos" or Rankings == "Tsuchinoe" or Rankings == "Hinoto" or Rankings == "Hinoe" or Rankings == "Kinoto" or Rankings == "Kinoe" or Rankings == "PseudoHashira" then
                     if game:GetService("Players").LocalPlayer.Quest.IsActive.Value == false then
                         game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = game:GetService("Workspace").IntNpcs.Mission.HumanoidRootPart.CFrame
                         wait(.3)
-                        game:GetService("ReplicatedStorage").Remotes.GiverQuest:FireServer("Mission")
+                        game:GetService("ReplicatedStorage").Remotes.GiverQuest:FireServer("SlayerMission")
                     end
                     if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Fists") then
                         game.Players.LocalPlayer.Character.Humanoid:EquipTool(game:GetService("Players").LocalPlayer.Backpack.Fists)
-                    elseif game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Old Nichirin Katana") then
+                    end
+                    if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Old Nichirin Katana") then
                         game.Players.LocalPlayer.Character.Humanoid:EquipTool(game:GetService("Players").LocalPlayer.Backpack["Old Nichirin Katana"])
-                    elseif game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Nichirin Katana") then
+                    end
+                    if game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Nichirin Katana") then
                         game.Players.LocalPlayer.Character.Humanoid:EquipTool(game:GetService("Players").LocalPlayer.Backpack["Nichirin Katana"])
                     end
                     if v.Name == "Demon" and v.Humanoid.Health > 0 then
                         repeat wait()
                         game:GetService("ReplicatedStorage").Remotes.CombatRemotes.Fists:FireServer("Release")
+                        game:GetService("ReplicatedStorage").Remotes.CombatRemotes.Katana:FireServer("Release")
                         game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.Velocity = Vector3.new(0,0,0)
                         game:GetService("Workspace").LivingThings[game.Players.LocalPlayer.Name].HumanoidRootPart.CFrame = v:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0,0,4)
                         until not Settings.autofarm or v.Humanoid.Health <= 0
                     end
                 end
-            end
             end
         end)
     end
