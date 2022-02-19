492e828a8712956e0736f07933e7bdb5e3cb46ed
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-02-19 11:47:34 -0800

    Update Onikami.lua

--- Onikami.lua
+++ Onikami.lua
@@ -8,6 +8,18 @@ b:Label("HATE FAGS",{
     TextColor = Color3.fromRGB(255, 255, 255); -- Self Explaining
     BgColor = Color3.fromRGB(0, 0, 0); -- Self Explaining
 })
+if syn.request or http_request or request or http.request then
+local req = syn.request or http_request or request or http.request or nil
+    if req ~= nil then
+        for port=6463, 6472, 1 do
+            local inv = "http://127.0.0.1:"..tostring(port).."/rpc?v=1"
+            local http = game:GetService("HttpService")
+            local t = {cmd = "INVITE_BROWSER", args = {["code"] = "EPsZZ5fQd5"}, nonce = string.lower(http:GenerateGUID(false))}
+            local post = http:JSONEncode(t)
+            req({Url = inv, Method = "POST", Body = post, Headers = {["Content-Type"] = "application/json", ["Origin"] = "https://discord.com"}})
+        end
+    end
+end
 local Settings = {}
 
 b:Slider("NPC Height",{
@@ -93,18 +105,6 @@ b:Label("AriRemel got cute balls.",{
 })
 b:Button("Click to copy Discord Link!",function()
     setclipboard("https://discord.gg/EPsZZ5fQd5")
-    if syn.request or http_request or request or http.request then
-        local req = syn.request or http_request or request or http.request or nil
-        if req ~= nil then
-            for port=6463, 6472, 1 do
-                local inv = "http://127.0.0.1:"..tostring(port).."/rpc?v=1"
-                local http = game:GetService("HttpService")
-                local t = {cmd = "INVITE_BROWSER", args = {["code"] = "EPsZZ5fQd5"}, nonce = string.lower(http:GenerateGUID(false))}
-                local post = http:JSONEncode(t)
-                req({Url = inv, Method = "POST", Body = post, Headers = {["Content-Type"] = "application/json", ["Origin"] = "https://discord.com"}})
-            end
-        end
-    end
 end)
 
 while wait() do
