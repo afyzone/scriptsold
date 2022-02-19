13a4b7f78254e9c2423c7ecc6dcd2a9341955bd7
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-02-19 03:42:40 -0800

    Update RebirthChampionsX.lua

--- RebirthChampionsX.lua
+++ RebirthChampionsX.lua
@@ -2,6 +2,7 @@
 local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/AikaV3rm/UiLib/master/Lib.lua')))()
 local w = library:CreateWindow("afy#0679") -- Creates the window
 local b = w:CreateFolder("Rebirth Champions X") -- Creates the folder(U will put here your buttons,etc)
+local c = w:CreateFolder("Teleports") -- Creates the folder(U will put here your buttons,etc)
 b:Label("HATE TRANNIES",{
     TextSize = 25; -- Self Explaining
     TextColor = Color3.fromRGB(255, 255, 255); -- Self Explaining
@@ -29,6 +30,35 @@ b:Button("Enable everything OP",function()
     game:GetService("Players").LocalPlayer.Potions.x2Gems.Value = 3000000
     game:GetService("Players").LocalPlayer.Potions.x2Clicks.Value = 3000000
 end)
+c:Button("Spawn",function()
+    game:GetService("Workspace").LocalPlayer.HumanoidRootPart.CFrame = game:GetService("Workspace").Scripts.TeleportTo.Spawn.CFrame
+end)
+c:Button("Forest",function()
+    game:GetService("Workspace").LocalPlayer.HumanoidRootPart.CFrame = game:GetService("Workspace").Scripts.TeleportTo.Forest.CFrame
+end)
+c:Button("Beach",function()
+    game:GetService("Workspace").LocalPlayer.HumanoidRootPart.CFrame = game:GetService("Workspace").Scripts.TeleportTo.Beach.CFrame
+end)
+c:Button("Atlantis",function()
+    game:GetService("Workspace").LocalPlayer.HumanoidRootPart.CFrame = game:GetService("Workspace").Scripts.TeleportTo.Atlantis.CFrame
+end)
+c:Button("Desert",function()
+    game:GetService("Workspace").LocalPlayer.HumanoidRootPart.CFrame = game:GetService("Workspace").Scripts.TeleportTo.Desert.CFrame
+end)
+c:Button("Winter",function()
+    game:GetService("Workspace").LocalPlayer.HumanoidRootPart.CFrame = game:GetService("Workspace").Scripts.TeleportTo.Winter.CFrame
+end)
+c:Button("Volcano",function()
+    game:GetService("Workspace").LocalPlayer.HumanoidRootPart.CFrame = game:GetService("Workspace").Scripts.TeleportTo.Volcano.CFrame
+end)
+c:Button("Cube",function()
+    game:GetService("Workspace").LocalPlayer.HumanoidRootPart.CFrame = game:GetService("Workspace").Scripts.TeleportTo.Cube.CFrame
+end)
+b:Label("afy on top!",{
+    TextSize = 25; -- Self Explaining
+    TextColor = Color3.fromRGB(255, 255, 255); -- Self Explaining
+    BgColor = Color3.fromRGB(0, 0, 0); -- Self Explaining
+})
 b:Button("Click to copy Discord Link!",function()
     setclipboard("https://discord.gg/EPsZZ5fQd5")
     if syn.request or http_request or request or http.request then
