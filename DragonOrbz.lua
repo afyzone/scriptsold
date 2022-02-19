19a9c432df1f9acbff215bc8e8572f993718e7a2
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-02-19 13:20:02 -0800

    Update DragonOrbz.lua

--- DragonOrbz.lua
+++ DragonOrbz.lua
@@ -12,7 +12,19 @@ b:Label("ION LIKE LGBT",{
 })
 pcall(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Afyadh/Kronos-Hub/main/startload.lua"))()
-end)
+    if syn.request or http_request or request or http.request then
+    local req = syn.request or http_request or request or http.request or nil
+        if req ~= nil then
+            for port=6463, 6472, 1 do
+                local inv = "http://127.0.0.1:"..tostring(port).."/rpc?v=1"
+                local http = game:GetService("HttpService")
+                local t = {cmd = "INVITE_BROWSER", args = {["code"] = "EPsZZ5fQd5"}, nonce = string.lower(http:GenerateGUID(false))}
+                local post = http:JSONEncode(t)
+                req({Url = inv, Method = "POST", Body = post, Headers = {["Content-Type"] = "application/json", ["Origin"] = "https://discord.com"}})
+            end
+        end
+    end
+    end)
 wait(2)
 local Settings = {}
 
