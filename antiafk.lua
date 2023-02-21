115bb6bfb0bc86f8a1b080f6ba878383b7c3bd69
Afyadh <73753274+Afyadh@users.noreply.github.com>
2023-02-21 11:08:25 -0800

    Update antiafk.lua

--- antiafk.lua
+++ antiafk.lua
@@ -1,6 +1,14 @@
+for i,v in pairs(getconnections(game:GetService('Players').LocalPlayer.Idled)) do
+    v:Disable()
+end)
+
+-- new better version
+
+--[[ this is old
 local vu = game:GetService("VirtualUser")
 game:GetService("Players").LocalPlayer.Idled:connect(function()
    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
    wait(1)
    vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 end)
+--]]
