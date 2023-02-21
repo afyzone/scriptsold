c5a37047a322c57680dc9478a5bf532d9930357c
Afyadh <73753274+Afyadh@users.noreply.github.com>
2023-02-21 11:11:41 -0800

    Delete xenoadminrejoin.lua

--- xenoadminrejoin.lua
+++
@@ -1,6 +0,0 @@
-local a = game.Players 
-while wait() do
-if a == "Inta_Kage" or a == "AwokenTy" or a == "EternalProwessKun" or a == "Vryomancerthen" or a == "RamenSaints"  or a == "ii_Justice" or a == "nbc77" or a == "ZenohTheAccused" then
-game:GetService("TeleportService"):Teleport(6797669494, game:GetService("Players").LocalPlayer)
-end
-end
